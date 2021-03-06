1919822e03e4cdcd1d4013b974bbf9308dcc9627
Carolina R <carolinarizk@gmail.com>
2021-03-05 22:19:20 -0500

    renamed type key and added total duration virtual property

--- models/workout.js
+++ models/workout.js
@@ -2,22 +2,37 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({
-  day: {
-    type: Date,
-    default: Date.now,
-  },
-  exercises: [
-    {
-      type: String,
-      name: String,
-      duration: Number,
-      weight: Number,
-      reps: Number,
-      sets: Number,
-      distance: Number,
+const WorkoutSchema = new Schema(
+  {
+    day: {
+      $type: Date,
+      default: Date.now,
     },
-  ],
+    exercises: [
+      {
+        type: String,
+        name: String,
+        duration: Number,
+        weight: Number,
+        reps: Number,
+        sets: Number,
+        distance: Number,
+      },
+    ],
+  },
+  {
+    typeKey: "$type",
+  }
+);
+
+WorkoutSchema.virtual("totalDuration").get(function () {
+  return this.exercises.reduce((accummulator, currentValue) => {
+    return accummulator + currentValue.duration;
+  }, 0);
+});
+
+WorkoutSchema.set("toJSON", {
+  virtuals: true,
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
